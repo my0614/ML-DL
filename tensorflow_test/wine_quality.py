ecc6a61389cd54a9a97c3f9650752d7e542b3f22
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-03-24 23:13:29 +0900

    Update wine_quality.py
    오타 해결 및 모델 생성 및 학습 오류

--- tensorflow_test/wine_quality.py
+++ tensorflow_test/wine_quality.py
@@ -28,14 +28,13 @@ print(wine['new_quality'].value_counts())
 
 del wine['quality']
 wine_norm = (wine-wine.min()) / (wine.max() - wine.min())
-print('wine_norm',wine_norm)
 
 wine_shuffle = wine_norm.sample(frac = 1)
 wine_np = wine_shuffle.to_numpy()
 
 train_idx = int(len(wine_np) * 0.8)
 train_X, train_Y = wine_np[:train_idx, :-1], wine_np[:train_idx, -1]
-test_X, test_Y = wine_np[train_idx : , :-1], wine_np[train_idx:, -1]
+test_X, test_Y = wine_np[train_idx: , :-1], wine_np[train_idx:, -1]
 train_Y = tf.keras.utils.to_categorical(train_Y, num_classes = 3) # num_classes = 3인 이유는 품질단계 0,1,2
 test_Y = tf.keras.utils.to_categorical(test_Y, num_classes = 3)
 
@@ -47,4 +46,6 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(units = 3, activation = 'softmax')
 ])
 
-model.compile(optimizer = tf.keras.optimizers.Adam(lr= 0.07), loss= 'categorical_crossentropy', metrics = 'accuray') # 분류의 metrics ->정확도
+model.compile(optimizer = tf.keras.optimizers.Adam(lr= 0.07), loss= 'categorical_crossentropy', metrics = ['accuracy']) # 분류의 metrics ->정확도
+
+history = model.fit(train_X, train_Y, epochs = 25, batch_size = 32, validation_split = 0.25)
