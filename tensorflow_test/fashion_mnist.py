6886f88c18e202a225dc3e2ec34aa33b76a28e21
Kim minyoung <aqi222@naver.com>
2021-03-25 16:51:18 +0900

    패션mnist정확도 계산

--- tensorflow_test/fashion_mnist.py
+++ tensorflow_test/fashion_mnist.py
@@ -29,4 +29,25 @@ model.compile(optimizer = tf.keras.optimizers.Adam(),
               metrics = ['accuracy'])
 
 model.summary()
+history = model.fit(train_X, train_Y, epochs = 25, validation_split= 0.25)
+
+#그래프
+plt.figure(figsize = (12,4))
+plt.subplot(1,2,1)
+plt.plot(history.history['loss'], 'b-', label = 'loss')
+plt.plot(history.history['val_loss'],'r--', label = 'val_loss')
+plt.xlabel('Epoch')
+plt.legend()
+
+plt.subplot(1,2,2)
+plt.plot(history.history['accuracy'], 'g--', label='accuracy')
+plt.plot(history.history['val_accuracy'], 'k--', label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylim(0.7,1)
+plt.legend()
+
+plt.show()
+
+result = model.evaluate(test_X, test_Y)
+print('정확도', result[1]* 100 ,'%')
 
