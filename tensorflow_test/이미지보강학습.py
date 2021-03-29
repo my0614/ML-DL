e46bf99b86d3911ad6ea1a542296968bae275fbf
Kim minyoung <aqi222@naver.com>
2021-03-29 11:55:14 +0900

    이미지 학습 보강데이터 합치기

--- tensorflow_test/이미지보강학습.py
+++ tensorflow_test/이미지보강학습.py
@@ -80,25 +80,53 @@ image_generator = ImageDataGenerator(
 augment_size = 30000
 randidx = np.random.randint(train_X.shape[0], size= augment_size)
 x_augmented = train_X[randidx].copy()
-Y_augmented = train_Y[randidx].copy()
+y_augmented = train_Y[randidx].copy()
 
 #flow()-> 보강된 이미지 생성
 #bitch_size -> 한번에 생성할 이미지 생성
-x_augmented = image_generator.flow(np.tile(train_X[0].reshape(28*28), 100).reshape(-1,28,28,1),
-                                  np.zeros(augment_size), batch_size = augment_size, shuffle = False).next()[0]
-
+x_augmented = image_generator.flow(x_augmented, np.zeros(augment_size), batch_size = augment_size, shuffle= False).next()[0]
 
 
 train_X = np.concatenate((train_X, x_augmented))
 train_Y = np.concatenate((train_Y, y_augmented))
 
 print(train_X.shape)
-'''
-import matplotlib.pyplot as plt
-plt.figure(figsize=(10,10))
-for i in range(100):
-    plt.subplot(10,10,i+1)
-    plt.axis('off')
-    plt.imshow(x_augmented[i].reshape(28,28), cmap = 'gray')
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(input_shape = (28,28,1), kernel_size = (3,3) , filters= 32,
+                           padding = 'same', activation='relu'),
+    tf.keras.layers.Conv2D(kernel_size = (3,3), filters = 64, padding ='same', activation = 'relu'),
+    tf.keras.layers.MaxPool2D(pool_size = (2,2)),
+    tf.keras.layers.Dropout(rate = 0.5),
+    tf.keras.layers.Conv2D(kernel_size = (3,3), filters = 128, padding = 'same', activation = 'relu'),
+    tf.keras.layers.Conv2D(kernel_size = (3,3), filters = 256, padding = 'valid', activation = 'relu'),
+    tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    tf.keras.layers.Dropout(rate = 0.5),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units = 512, activation = 'relu'),
+    tf.keras.layers.Dropout(rate = 0.5),
+    tf.keras.layers.Dense(units = 256, activation = 'relu'),
+    tf.keras.layers.Dropout(rate = 0.5),
+    tf.keras.layers.Dense(units = 10, activation='softmax')
+])
+
+model.compile(optimizer =tf.keras.optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics = ['accuracy'])
+history = model.fit(train_X, train_Y, epochs = 25, validation_split = 0.25)
+
+plt.figure(figsize=(12,4))
+plt.subplot(1,2,1)
+plt.plot(history.history['loss'], 'b-',label = 'loss')
+plt.plot(history.history['val_loss'], 'r--',label = 'val_loss')
+plt.xlabel('Epoch')
+plt.legend()
+
+plt.subplot(1,2,2)
+plt.plot(history.history['accuracy'], 'g-', label = 'accuracy')
+plt.plot(history.history['val_accuracy'], 'k--', label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylim(0.7,1)
+plt.legend()
+
 plt.show()
-'''
+
+model.evaluate(test_X, test_Y, verbose = 0)
