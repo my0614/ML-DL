c911ba020c330a26b469a8f5cbefda4c2c86424e
Kim minyoung <aqi222@naver.com>
2021-04-18 16:32:14 +0900

    데이터세트 shuffle,batch설정하기

--- tensorflow_test/분석/조선왕조실록/조선왕조실록.py
+++ tensorflow_test/분석/조선왕조실록/조선왕조실록.py
@@ -21,7 +21,7 @@ def clean_str(string):
     string = re.sub(r"\'","", string)
    
     return string
-#
+train_text = train_text.split(' ')
 train_text = [clean_str(sentence) for sentence in train_text]
 
 train_text_X = []
@@ -45,15 +45,12 @@ text_as_int = np.array([word2idx[c] for c in train_text_X]) #단어에 대한 �
 print('{')
 
 # 단어 시퀸스정하기
+
 for word,_ in zip(word2idx, range(10)):
     print(' {:4s}: {:3d},'.format(repr(word), word2idx[word]))
 print(' ...\n}')
 print('index of UNK: {}'.format(word2idx['UNK']))
-
 print(train_text_X[:20])
-print(text_as_int[:20])
-
-seq_length = 25 # 단어 시퀸스 최대길이
 examples_per_epoch = len(text_as_int)
 print(examples_per_epoch)
 sentence_dataset = tf.data.Dataset.from_tensor_slices(text_as_int) #데이터셋 생성
@@ -61,8 +58,6 @@ sentence_dataset = sentence_dataset.batch(seq_length +1, drop_remainder= True)
 for item in sentence_dataset.take(1):
     print(idx2word[item.numpy()])
     print(item.numpy())
-
-#다음에 나올 글자 예측하기
 def split_input_target(chunk):
     return [chunk[:-1], chunk[-1]]
 
@@ -72,3 +67,48 @@ for x,y in train_dataset.take(1):
     print(x.numpy())
     print(idx2word[y.numpy()])
     print(y.numpy())
+
+#데이터세트 shufle, batch 설정
+BATCH_SIZE = 128
+steps_per_epoch = examples_per_epoch
+BUFFER_SIZE = 10000
+
+train_dataset = train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder =True)
+# 모델 정의하기
+total_words = len(vocab)
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(total_words, 100, input_length = seq_length),
+    tf.keras.layers.LSTM(units= 100, return_sequences = True),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.LSTM(units = 100),
+    tf.keras.layers.Dense(total_words, activation = 'softmax')
+])
+model.compile(optimizer = 'adam', loss = 'sparse_categorital_crossentropy', metrics = ['accuracy'])
+model.summary()
+
+#모델학습하기
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+def testmodel(epoch, logs):
+    if epoch % 5 != 0 and epoch != 49:
+        return
+    test_sentence - train_text[0]
+    
+    next_words = 100
+    for _ in range(next_words):
+        test_Text_X = test_sentence.split(' ')[-seq_length:]
+        test_text_X = np.array([word2idx[c] if c in word2idx else word2idx['UNK'] for c in test_text_X])
+        test_text_X = pad_sequences([test_text_X], maxlen=seq_length, padding = 'pre', value = word2idx['UNK'])
+        
+        output_idx = model.predict_classes(test_text_X)
+        test_sentence += ' ' + idx2word[output_idx[0]]
+        
+    print()
+    print('hi')
+    print(test_sentence)
+    print()
+testmodelcb = tf.keras.callbacks.LambdaCallback(on_epoch_end = testmodel)
+history = model.fit(train_dataset.repeat(), epochs = 50, steps_per_epoch = steps_per_epoch ,callbacks = [testmodelcb],verbose = 2)
+
+        
+    
